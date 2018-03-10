59e343a989ecd18f7b6a42099ecb4198997d094d
Feng Luan <feng.luan@sesam.no>
2018-03-10 13:14:41 +0100

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+NAME=aluan
+if [ ! "$(docker ps -q -f name=$NAME)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=$NAME)" ]; then
+        docker rm $NAME
+    fi
+    docker run -d -p 8887:8888 --name $NAME\
+      -v ~/Github/PythonProjects/alu/:/home/jovyan/alu \
+      jupyter/datascience-notebook start-notebook.sh --NotebookApp.token=''
+fi
