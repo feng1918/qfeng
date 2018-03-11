4ac6ab6cb52d1ed770926cca7617748b2104fd04
Feng Luan <feng.luan@sesam.no>
2018-03-11 09:15:58 +0100

    Using qfeng/zipline docker

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ if [ ! "$(docker ps -q -f name=$NAME)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=$NAME)" ]; then
         docker rm $NAME
     fi
-    docker run -d -p 8887:8888 --name $NAME\
+    docker run -d -p 8887:8888 --name $NAME \
       -v ~/Github/PythonProjects/alu/:/home/jovyan/alu \
-      jupyter/datascience-notebook start-notebook.sh --NotebookApp.token=''
+      -v ~/Github/PythonProjects/alu/zipline:/home/jovyan/.zipline \
+      qfeng/zipline start-notebook.sh --NotebookApp.token=''
 fi
